cdd4026dec0bdbaf1fc265986234893f08dde1f0
xiepan <406551408@qq.com>
2017-07-22 17:15:10 +0800

    能够分页显示数据库中的数据，html中实现超链接

--- depot/depotapp/urls.py
+++ depot/depotapp/urls.py
@@ -4,8 +4,9 @@ from .views import *
 
 
 urlpatterns = [
+    url(r'^product/$',base_product),
     url(r'^product/create/$', create_product),
-    #url(r'^product/list/$', list_product ),
+    url(r'^product/list/$', list_product ),
     #url(r'^product/edit/(?P[^/]+)/$', edit_product),
     #url(r'^product/view/(?P[^/]+)/$', view_product),
 ]
--- depot/depotapp/views.py
+++ depot/depotapp/views.py
@@ -13,18 +13,38 @@ from .models import *
 from .forms import *
 
 
+def base_product(request):
+    return HttpResponse(request,'depotapp\base_product.html')
+
 def create_product(request):
-    if request.method == 'POST':        
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            form.save()
-            form = ProductForm()
+    
+    form = ProductForm(request.POST or None)
+    #if request.method == 'POST':        
+    #   form = ProductForm(request.POST)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
 
     #t = get_template('depotapp/create_product.html')
     #c = RequestContext(request,locals())
-    return render(request,'depotapp/creare_procuct.html',{'form':form})#HttpResponse(t.render(c))
+    return render(request,'depotapp\create_product.html',{'item':form})#HttpResponse(t.render(c))
 
-
+def list_product(request):
+    list_items = Product.objects.all().values('name','price')
+    paginator = Paginator(list_items,5)
+    
+    try:
+        page = int(request.GET.get('page','1'))
+    except ValueError:
+        page = 1
+        
+    try:
+        list_items = paginator.page(page)
+    except:
+        list_items = paginator.page(paginator.num_pages)
+        
+    return render(request,'depotapp\list_product.html',{'items':list_items})
+    
 '''
 def list_product(request):
   
