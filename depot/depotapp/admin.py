2d4d1e3623e9beabfdeeb82fdabbc01967577c64
xiepan <406551408@qq.com>
2017-07-23 15:29:17 +0800

    在admin中添加了fieldset，filter，list_display等属性

--- depot/depotapp/admin.py
+++ depot/depotapp/admin.py
@@ -3,4 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 
-admin.site.register(models.Product)
+class BaseAdmin(admin.ModelAdmin):
+	list_displa=('name','price')#指定要显示的字段
+	search_fields=('name',)#指定要搜索的字段，将会出现一个搜索框让管理员搜索关键字
+	list_filter=('name',)
+	'''指定列表过滤器，右边将出现一个快捷的日期过滤选项，
+	以便开发人员快速定位想要的数据，同样你也可以指定非日期类的字段
+	'''
+	'''
+	#分组表单,当有多个表单相关联时可以使用这个属性
+	fieldsets=(
+	()，
+	)
+	'''
+admin.site.register(models.Product,BaseAdmin)
--- depot/depotapp/models.py
+++ depot/depotapp/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100,verbose_name='产品名称')
     price = models.CharField(max_length=50,verbose_name='产品价格')
+    def __unicode__(self):
+        return self.name
+    class Meta:
+        verbose_name = '产品列表'
+        verbose_name_plural= '产品清单'
