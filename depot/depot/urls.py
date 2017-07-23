a1add079f9b4a2657b45436848051f667781107e
xiepan <406551408@qq.com>
2017-07-23 15:00:59 +0800

    将models中的Product加入到admin中，可以利用自带的管理工具进行项目管理

--- depot/depot/urls.py
+++ depot/depot/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
 	
 	url(r'^depotapp/',include('depotapp.urls')),
 ]
